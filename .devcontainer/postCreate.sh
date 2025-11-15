41f2a0e2d02ce87c128bcbf98bf8e2be6214a331
OscarQuintana05 <oscar.quintana.acosta@udla.edu.ec>
2025-11-15 22:15:01 +0000

    fix: postCreate script content and make executable

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,8 @@
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y sl
+sudo apt-get install sl
 echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
 echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
